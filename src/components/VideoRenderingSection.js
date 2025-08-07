d8982cb10013f04470c7867fe3f50ac2614896da
nganlinh4 <nganlinh4@gmail.com>
2025-08-07 17:01:27 +0900

    refactor: enhance video file display with icon and improve layout for multi-line content

--- src/components/VideoRenderingSection.js
+++ src/components/VideoRenderingSection.js
@@ -1328,6 +1328,9 @@ const VideoRenderingSection = ({
               <h4>{t('videoRendering.videoInput', 'Video Input')}</h4>
               {selectedVideoFile ? (
                 <div className="selected-video-info">
+                  <svg className="video-file-icon" width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M17 10.5V7C17 5.89543 16.1046 5 15 5H5C3.89543 5 3 5.89543 3 7V17C3 18.1046 3.89543 19 5 19H15C16.1046 19 17 18.1046 17 17V13.5L21 17.5V6.5L17 10.5Z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+                  </svg>
                   <span className="video-name">
                     {(selectedVideoFile instanceof File ? selectedVideoFile.name :
                       (selectedVideoFile.name || selectedVideoFile.title || 'Current Video'))}
