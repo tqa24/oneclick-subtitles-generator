06d7b375cedc081a138dd62cdedb4e1715124203
nganlinh4 <nganlinh4@gmail.com>
2025-10-17 16:34:09 +0900

    feat: replace SVG icons with Material Symbols in VideoBottomControls and VideoTopsideButtons for improved consistency

--- src/components/previews/VideoBottomControls.js
+++ src/components/previews/VideoBottomControls.js
@@ -493,19 +493,19 @@ const VideoBottomControls = ({
                           >
                             {isMuted || volume === 0 ? (
                               // Muted icon
-                              <svg width="18" height="18" viewBox="0 0 24 24" fill="white" style={{ filter: 'drop-shadow(0 1px 2px rgba(0, 0, 0, 0.8))' }}>
-                                <path d="M16.5 12c0-1.77-1.02-3.29-2.5-4.03v2.21l2.45 2.45c.03-.2.05-.41.05-.63zm2.5 0c0 .94-.2 1.82-.54 2.64l1.51 1.51C20.63 14.91 21 13.5 21 12c0-4.28-2.99-7.86-7-8.77v2.06c2.89.86 5 3.54 5 6.71zM4.27 3L3 4.27 7.73 9H3v6h4l5 5v-6.73l4.25 4.25c-.67.52-1.42.93-2.25 1.18v2.06c1.38-.31 2.63-.95 3.69-1.81L19.73 21 21 19.73l-9-9L4.27 3zM12 4L9.91 6.09 12 8.18V4z"/>
-                              </svg>
+                              <span className="material-symbols-rounded" style={{ color: 'white', fontSize: 18, textShadow: '0 1px 2px rgba(0, 0, 0, 0.8)', fontFamily: "'Material Symbols Rounded', 'Material Symbols'", fontVariationSettings: '"wght" 700, "GRAD" 200, "opsz" 24', display: 'inline-block' }}>
+                                volume_off
+                              </span>
                             ) : volume < 0.5 ? (
                               // Low volume icon
-                              <svg width="18" height="18" viewBox="0 0 24 24" fill="white" style={{ filter: 'drop-shadow(0 1px 2px rgba(0, 0, 0, 0.8))' }}>
-                                <path d="M3 9v6h4l5 5V4L7 9H3zm13.5 3c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02z"/>
-                              </svg>
+                              <span className="material-symbols-rounded" style={{ color: 'white', fontSize: 18, textShadow: '0 1px 2px rgba(0, 0, 0, 0.8)', fontFamily: "'Material Symbols Rounded', 'Material Symbols'", fontVariationSettings: '"wght" 700, "GRAD" 200, "opsz" 24', display: 'inline-block' }}>
+                                volume_down
+                              </span>
                             ) : (
                               // High volume icon
-                              <svg width="18" height="18" viewBox="0 0 24 24" fill="white" style={{ filter: 'drop-shadow(0 1px 2px rgba(0, 0, 0, 0.8))' }}>
-                                <path d="M3 9v6h4l5 5V4L7 9H3zm13.5 3c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02zM14 3.23v2.06c2.89.86 5 3.54 5 6.71s-2.11 5.85-5 6.71v2.06c4.01-.91 7-4.49 7-8.77s-2.99-7.86-7-8.77z"/>
-                              </svg>
+                              <span className="material-symbols-rounded" style={{ color: 'white', fontSize: 18, textShadow: '0 1px 2px rgba(0, 0, 0, 0.8)', fontFamily: "'Material Symbols Rounded', 'Material Symbols'", fontVariationSettings: '"wght" 700, "GRAD" 200, "opsz" 24', display: 'inline-block' }}>
+                                volume_up
+                              </span>
                             )}
                           </div>
                         </div>
@@ -666,9 +666,9 @@ const VideoBottomControls = ({
                               }
                             }}
                           >
-                            <svg width="18" height="18" viewBox="0 0 24 24" fill="white" style={{ filter: 'drop-shadow(0 1px 2px rgba(0, 0, 0, 0.8))' }}>
-                              <path d="M19 9h-4V3H9v6H5l7 7 7-7zM5 18v2h14v-2H5z"/>
-                            </svg>
+                            <span className="material-symbols-rounded" style={{ color: 'white', fontSize: 18, textShadow: '0 1px 2px rgba(0, 0, 0, 0.8)', fontFamily: "'Material Symbols Rounded', 'Material Symbols'", fontVariationSettings: '"wght" 700, "GRAD" 200, "opsz" 24', display: 'inline-block' }}>
+                              download
+                            </span>
                           </div>
 
                           {/* Picture-in-Picture Button */}
@@ -699,9 +699,9 @@ const VideoBottomControls = ({
                               }
                             }}
                           >
-                            <svg width="18" height="18" viewBox="0 0 24 24" fill="white" style={{ filter: 'drop-shadow(0 1px 2px rgba(0, 0, 0, 0.8))' }}>
-                              <path d="M19 7h-8v6h8V7zm2-4H3c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h18c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H3V5h18v14z"/>
-                            </svg>
+                            <span className="material-symbols-rounded" style={{ color: 'white', fontSize: 18, textShadow: '0 1px 2px rgba(0, 0, 0, 0.8)', fontFamily: "'Material Symbols Rounded', 'Material Symbols'", fontVariationSettings: '"wght" 700, "GRAD" 200, "opsz" 24', display: 'inline-block' }}>
+                              picture_in_picture_alt
+                            </span>
                           </div>
                         </div>
                       </LiquidGlass>
@@ -759,14 +759,14 @@ const VideoBottomControls = ({
                       }}>
                         {isFullscreen ? (
                           // Exit fullscreen icon
-                          <svg width="20" height="20" viewBox="0 0 24 24" fill="white" style={{ filter: 'drop-shadow(0 1px 2px rgba(0, 0, 0, 0.8))' }}>
-                            <path d="M5 16h3v3h2v-5H5v2zm3-8H5v2h5V5H8v3zm6 11h2v-3h3v-2h-5v5zm2-11V5h-2v5h5V8h-3z"/>
-                          </svg>
+                          <span className="material-symbols-rounded" style={{ color: 'white', fontSize: 20, textShadow: '0 1px 2px rgba(0, 0, 0, 0.8)', fontFamily: "'Material Symbols Rounded', 'Material Symbols'", fontVariationSettings: '"wght" 700, "GRAD" 200, "opsz" 24', display: 'inline-block' }}>
+                            fullscreen_exit
+                          </span>
                         ) : (
                           // Fullscreen icon
-                          <svg width="20" height="20" viewBox="0 0 24 24" fill="white" style={{ filter: 'drop-shadow(0 1px 2px rgba(0, 0, 0, 0.8))' }}>
-                            <path d="M7 14H5v5h5v-2H7v-3zm-2-4h2V7h3V5H5v5zm12 7h-3v2h5v-5h-2v3zM14 5v2h3v3h2V5h-5z"/>
-                          </svg>
+                          <span className="material-symbols-rounded" style={{ color: 'white', fontSize: 20, textShadow: '0 1px 2px rgba(0, 0, 0, 0.8)', fontFamily: "'Material Symbols Rounded', 'Material Symbols'", fontVariationSettings: '"wght" 700, "GRAD" 200, "opsz" 24', display: 'inline-block' }}>
+                            fullscreen
+                          </span>
                         )}
                       </div>
                     </LiquidGlass>
--- src/components/previews/VideoTopsideButtons.js
+++ src/components/previews/VideoTopsideButtons.js
@@ -449,9 +449,9 @@ const VideoTopsideButtons = ({
             ) : (
               // Show refresh icon when not refreshing
               <>
-                <svg width="22" height="22" viewBox="0 0 24 24" fill="white" style={{ filter: 'drop-shadow(0 1px 2px rgba(0, 0, 0, 0.8))' }}>
-                  <path d="M17.65 6.35C16.2 4.9 14.21 4 12 4c-4.42 0-7.99 3.58-7.99 8s3.57 8 7.99 8c3.73 0 6.84-2.55 7.73-6h-2.08c-.82 2.33-3.04 4-5.65 4-3.31 0-6-2.69-6-6s2.69-6 6-6c1.66 0 3.14.69 4.22 1.78L13 11h7V4l-2.35 2.35z" />
-                </svg>
+                <span className="material-symbols-rounded" style={{ color: 'white', fontSize: 22, textShadow: '0 1px 2px rgba(0, 0, 0, 0.8)', fontFamily: "'Material Symbols Rounded', 'Material Symbols'", fontVariationSettings: '"wght" 700, "GRAD" 200, "opsz" 24', display: 'inline-block' }}>
+                  refresh
+                </span>
                 <span style={{ color: 'white', fontSize: '13px', fontWeight: '600', textShadow: '0 1px 2px rgba(0, 0, 0, 0.8)' }}>
                   {t('preview.refreshNarration', 'Refresh Narration')}
                 </span>
@@ -500,9 +500,9 @@ const VideoTopsideButtons = ({
               minWidth: 'fit-content'
             }}
           >
-            <svg width="24" height="24" viewBox="0 0 24 24" fill="white" style={{ filter: 'drop-shadow(0 1px 2px rgba(0, 0, 0, 0.8))' }}>
-              <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-6h2v6zm0-8h-2V7h2v2z"/>
-            </svg>
+            <span className="material-symbols-rounded" style={{ color: 'white', fontSize: 24, textShadow: '0 1px 2px rgba(0, 0, 0, 0.8)', fontFamily: "'Material Symbols Rounded', 'Material Symbols'", fontVariationSettings: '"wght" 700, "GRAD" 200, "opsz" 24', display: 'inline-block' }}>
+              info
+            </span>
             </div>
           </LiquidGlass>
         )}
@@ -597,9 +597,9 @@ const VideoTopsideButtons = ({
             minWidth: 'fit-content',
             whiteSpace: 'nowrap'
           }}>
-            <svg width="20" height="20" viewBox="0 0 24 24" fill="white" style={{ filter: 'drop-shadow(0 1px 2px rgba(0, 0, 0, 0.8))' }}>
-              <path d="M21 19V5c0-1.1-.9-2-2-2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2zM8.5 13.5l2.5 3.01L14.5 12l4.5 6H5l3.5-4.5z"/>
-            </svg>
+            <span className="material-symbols-rounded" style={{ color: 'white', fontSize: 20, textShadow: '0 1px 2px rgba(0, 0, 0, 0.8)', fontFamily: "'Material Symbols Rounded', 'Material Symbols'", fontVariationSettings: '"wght" 700, "GRAD" 200, "opsz" 24', display: 'inline-block' }}>
+              image
+            </span>
             <span style={{ color: 'white', fontSize: '13px', fontWeight: '600', textShadow: '0 1px 2px rgba(0, 0, 0, 0.8)' }}>
               {t('preview.extractFrame', 'Extract Frame')}
             </span>
@@ -689,9 +689,9 @@ const VideoTopsideButtons = ({
             ) : (
               // Material Design download icon
               <>
-                <svg width="20" height="20" viewBox="0 0 24 24" fill="white" style={{ filter: 'drop-shadow(0 1px 2px rgba(0, 0, 0, 0.8))' }}>
-                  <path d="M19 9h-4V3H9v6H5l7 7 7-7zM5 18v2h14v-2H5z" />
-                </svg>
+                <span className="material-symbols-rounded" style={{ color: 'white', fontSize: 20, textShadow: '0 1px 2px rgba(0, 0, 0, 0.8)', fontFamily: "'Material Symbols Rounded', 'Material Symbols'", fontVariationSettings: '"wght" 700, "GRAD" 200, "opsz" 24', display: 'inline-block' }}>
+                  download
+                </span>
                 <span style={{ color: 'white', fontSize: '13px', fontWeight: '600', textShadow: '0 1px 2px rgba(0, 0, 0, 0.8)' }}>
                   {t('preview.downloadAudio', 'Download Audio')}
                 </span>
